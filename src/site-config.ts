1ff7fe4f3750f03517a3fa7f75dfdc3f013753e2
Steve Hansen <stvnhnsn@gmail.com>
2026-08-09 19:25:44 -0500

    First iteration of JK Contracting

--- src/site-config.ts
+++ src/site-config.ts
@@ -1,3 +1,8 @@
-export const BUSINESS_NAME = "Stone Ridge Designs";
-export const CONTACT_PHONE = "(555) 123-4567";
-export const CONTACT_CITY_STATE = "Green Bay, WI";
+import logo from "./assets/logo/jk-contracting-transparent.png"; // Import file directly as asset module rather than using string URL
+//import logo from "./assets/logo/FullLogo.jpg"; // Import file directly as asset module rather than using string URL
+
+export const BUSINESS_LOGO = logo;
+export const BUSINESS_NAME = "JK Contracting";
+export const FULL_BUSINESS_NAME = "JK Contracting, LLC";
+export const CONTACT_PHONE = "(906) 869-9974";
+export const CONTACT_CITY_STATE = "Marquette, MI";
