c8694cbd6367818d0b4a85cdf827b62e15dcd8bb
Steve Hansen <stvnhnsn@gmail.com>
2026-08-12 12:00:37 -0500

    Added trust bar

--- src/site-config.ts
+++ src/site-config.ts
@@ -1,7 +1,11 @@
-import logo from "./assets/logo/jk-contracting-transparent.png"; // Import file directly as asset module rather than using string URL
-//import logo from "./assets/logo/FullLogo.jpg"; // Import file directly as asset module rather than using string URL
+// Import files directly as asset modules rather than using string URLs
+import businessLogo from "./assets/logos/jk-contracting-transparent.png";
+import hbaUpLogo from "./assets/logos/hba-up-logo.png";
+import licensedInsuredLogo from "./assets/logos/licensed-insured-badge.png";
 
-export const BUSINESS_LOGO = logo;
+export const BUSINESS_LOGO = businessLogo;
+export const HBA_UP_LOGO = hbaUpLogo;
+export const LICENSED_INSURED_LOGO = licensedInsuredLogo;
 export const BUSINESS_NAME = "JK Contracting";
 export const FULL_BUSINESS_NAME = "JK Contracting, LLC";
 export const CONTACT_PHONE = "(906) 869-9974";
